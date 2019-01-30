2877b3e4720cfd4c2c00c5e57577f4b778576c62
Quan T. Nguyen <quan.nguyen@eastgate-software.com>
2019-01-30 18:27:51 +0700

    Add HelloCaliburn project

---
+++ HelloCaliburn/HelloBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+
+namespace HelloCaliburn
+{
+    class HelloBootstrapper : BootstrapperBase
+    {
+        public HelloBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
